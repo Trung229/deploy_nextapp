53b3fb633250ece169fbc52752ff77a60f8f48fc
Pham Trung <phamtrung@Mac-mini-cua-Pham.local>
2022-06-21 11:11:28 +0700

    test login

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,11 @@
+describe('Test get images', () => {
+    it('passes', () => {
+        cy.visit('https://example.cypress.io')
+        cy.get('a')
+        .contains("type")
+        .click()
+        cy.get(".action-email")
+        .type("trungpham@gmail.com")
+        .should('have.value','trungpham@gmail.com')
+    })
+  })
