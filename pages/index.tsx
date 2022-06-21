2418eb610291d9bda0a13de14da59c694ceb5a1a
Pham Trung <phamtrung@Mac-mini-cua-Pham.local>
2022-06-21 14:36:02 +0700

    login with docker hub

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,35 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { withSentry } from '@sentry/nextjs';
 
-
-export default function Home() {
+const Home = () => {
+  console.log("===>", withSentry);
+  const a: number = 1;
   return (
-    <div >
-      Enter
-      <h2>Welcome to my</h2>
+    <>
+      <Head>
+        <script
+          src="https://browser.sentry-cdn.com/7.2.0/bundle.min.js"
+          integrity="sha384-yvW0r7aI4VkwNfs/eOzYsODvXkNVQon3MBtow61jPf/pOR166EPvTSNBr9nG3C3y"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <div >
+        Enter
+        <h2>Welcome to my {a}</h2>
 
-      <form>
-        <label aria-label="yahalo" className="ok">yahalo</label>
-        <input type="text" name="yahalo" value="John" />
-      </form>
-    </div >
+        <form>
+          <label aria-label="yahalo" className="ok">yahalo</label>
+          <input onChange={() => { }} type="text" name="yahalo" value="John" />
+        </form>
+        <button type="button" onClick={() => {
+          throw new Error("Sentry Frontend Error");
+        }}>
+          Throw error
+        </button>
+      </div >
+    </>
   )
 }
+
+export default Home
