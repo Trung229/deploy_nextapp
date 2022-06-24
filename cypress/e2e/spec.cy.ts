00598fca4477dab40f006bfb0100ad3e9e2375f6
Pham Trung <phamtrung@Mac-mini-cua-Pham.local>
2022-06-24 17:13:44 +0700

    update

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,35 @@
+import {add, minus, multi, divide} from '../../helper/math'
+
 describe('Test call video', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
     cy.get('a')
   .contains('get')
+  })
+})
+
+describe('Unit Test Application Code', function () {
+
+  before(() => {
+    // check if the import worked correctly
+    expect(add, 'add').to.be.a('function')
+  })
+
+  context('math.js', function () {
+    it('can add numbers', function () {
+      expect(add(1, 2)).to.eq(3)
+    })
+
+    it('can subtract numbers', function () {
+      expect(minus(5, 12)).to.eq(-7)
+    })
+
+    it('can divide numbers', function () {
+      expect(divide(27, 9)).to.eq(3)
+    })
+
+    it('can muliple numbers', function () {
+      expect(multi(5, 4)).to.eq(20)
+    })
   })
 })
